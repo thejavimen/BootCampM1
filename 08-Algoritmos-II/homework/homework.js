a053a2b950a383a3332149f6d78eb6501e0fee46
thejavimen <thejavimen@gmail.com>
2022-06-16 00:16:33 -0500

    16junio2022_00:16

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,22 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+if(array.length<2){
+    return array;
+  }
+  let x=array.pop();
+  let idx=0;
+  let ma=[];
+  let me=[];
+  while(idx<array.length){
+    if(x<array[idx]){
+      ma.push(array[idx]);
+    }else{
+      me.push(array[idx]);
+    }
+    idx++;
+  }
+  return quickSort(me).concat(x,quickSort(ma));
 }
 
 function mergeSort(array) {
@@ -14,9 +29,50 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length<2){
+    return array;
+  }
+  let mid=Math.floor((array.length)/2);
+  let izq=array.slice(0,mid);
+  let der=array.slice(mid,array.length);
+  return ordenar(mergeSort(izq),mergeSort(der));
 }
 
+function ordenar(aIzq,aDer){
+  let vec=[];
+  let idx=0;
+  let idx2=0;
+  while(idx!=1000){
+    if(aIzq[idx]!=null){
+      if(aDer[idx]!=null){
+        if(aIzq[idx]>aDer[idx2]){
+          vec.push(aDer[idx2]);
+          aDer.shift();
+        }else{
+          vec.push(aIzq[idx]);
+          aIzq.shift();
+          idx2=0;
+        }
+      }else{
+        
+        if(aIzq[idx]!=null){
+          vec=vec.concat(aIzq);
+          idx=1000;
+        }else{
+          idx=1000;
+        }
+      }
+    }else{
+      if(aDer[idx]!=null){
+        vec=vec.concat(aDer);
+        idx=1000;
+      }else{
+        idx=1000;
+      }
+    }
+  }
+  return vec;
+}
 // No modificar nada debajo de esta línea
 // --------------------------------
 
